a2c52f4045a8d48ed2c47fdb7c55a3503d79d68f
Javier Montero <monterojaviel18@gmail.com>
2024-09-23 19:30:39 -0400

    Add login function for authentication tests

---
+++ tests/authentication/login.ts
@@ -0,0 +1,19 @@
+import { Page } from "@playwright/test";
+
+// Esta función puede ser reutilizada en varias pruebas
+export async function login(
+  page1Promise: Promise<Page>,
+  username: string,
+  password: string
+) {
+  const page1 = await page1Promise;
+  await page1.getByPlaceholder("Email, phone, or Skype").click();
+  await page1.getByPlaceholder("Email, phone, or Skype").fill(username);
+  await page1.getByRole("button", { name: "Next" }).click();
+  await page1.getByPlaceholder("Password").click();
+  await page1.getByPlaceholder("Password").fill(password);
+  await page1.getByRole("button", { name: "Sign in" }).click();
+  await page1.getByRole("button", { name: "Yes" }).click();
+
+  await page1.waitForTimeout(2000);
+}
